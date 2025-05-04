853500c39b4935907a6a84904f035416a54ddc12
Olawale <olapetanwale@outlook.com>
2025-05-04 20:32:38 +0100

    Add testdata file to .gitignore and mask udid

--- src/test/java/utils/DeviceCaps.java
+++ src/test/java/utils/DeviceCaps.java
@@ -8,7 +8,7 @@ public class DeviceCaps extends TestBase {
 
     public static void samsungA32() {
         caps = new DesiredCapabilities();
-        caps.setCapability("udid", "RZ8T30532KZ");
+        caps.setCapability("udid", testData.getProperty("samsungA32Udid"));
         caps.setCapability("automationName", "UiAutomator2");
         caps.setCapability("platformVersion", "13.0");
         caps.setCapability("deviceName", "samsung SM-A325F");
@@ -29,7 +29,7 @@ public class DeviceCaps extends TestBase {
     }
     public static void Camon18() {
         caps = new DesiredCapabilities();
-        caps.setCapability("udid", "07312251BH102898");
+        caps.setCapability("udid", testData.getProperty("camon18Udid"));
         caps.setCapability("automationName", "UiAutomator2");
 //        caps.setCapability("platformVersion", "13.0");
         caps.setCapability("app", System.getProperty("user.dir") + "/src/test/java/Resources/Android-MyDemoAppRN.1.3.0.build-244.apk");
